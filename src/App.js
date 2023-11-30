d5fac82e53a10d9c12274faa651a1197068de681
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-30 19:46:19 +0430

    Refactor routes and components in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
+import RequireAuth from './components/RequireAuth';
+import AddCarsForm from './components/cars/AddCarsForm';
+// import Navbar from './components/Navbar';
+import MainPage from './components/cars/MainPage';
+import DeleteCar from './components/cars/DeleteCar';
+import Reservations from './components/reservations/Reservations';
+import CarDetails from './components/cars/CarDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  // <div className="container-fluid main-height ">
+  // <main className="col">
+  <>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<RequireAuth />}>
+        <Route exact path="/" element={<AddCarsForm />} />
+        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/details/:id" element={<CarDetails />} />
+        <Route path="/cars/delete" element={<DeleteCar />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Route>
+    </Routes>
+  </>
+  // </main>
+  // </div>
+);
 
 export default App;
