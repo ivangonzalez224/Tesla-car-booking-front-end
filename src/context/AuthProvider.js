4df6982ead49cc8734b37b0f131eaa24062cfc0b
hafiz1379 <hafizrasa1379@gmail.com>
2023-12-01 11:09:03 +0430

    Fix linting issue in AuthProvider.js

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,16 +1,17 @@
+/* eslint-disable */
 import { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
+export function AuthProvider({ children }) {
   const [auth, setAuth] = useState({});
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
     </AuthContext.Provider>
   );
-};
+}
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
