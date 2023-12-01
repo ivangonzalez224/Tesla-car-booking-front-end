c7ef1eb01d6242c882f695c325eff312174f8595
Bruno Kambere <brunokambere@gmail.com>
2023-12-02 00:26:55 +0200

    Load and didplays the list of all cars

--- src/components/cars/MainPage.jsx
+++ src/components/cars/MainPage.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { React } from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from '../Navbar';
+import CarItem from './CarItem';
+import '../../assets/css/carousel.css';
+
 
 function MainPage() {
+
+  const { cars } = useSelector((state) => state.cars);
+
   return (
     <>
       <Navbar />
       <div className="main-page-container">MainPage</div>
+      {cars.map((car) => (
+        <CarItem
+          key={car.id}
+          car={car}
+          classNames={{
+            button: 'btn btn-outline-primary',
+            carBody: 'card-body',
+            imageContainer: 'image-container',
+            image: 'card-img-top',
+            title: 'card-title',
+            discription: 'card-text',
+          }}
+        />
+      ))}
     </>
   );
 }
