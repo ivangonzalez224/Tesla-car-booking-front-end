a0ca79e3e9bc2b8bcd2330d4bcf6786f4952dd59
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-05 17:09:38 -0500

    Update the DeleteCar component to show a table with the available cars list

--- src/components/cars/DeleteCar.jsx
+++ src/components/cars/DeleteCar.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from '../Navbar';
+import '../../assets/css/deleteCar.css';
 
 function DeleteCar() {
+  const { cars } = useSelector((state) => state.cars);
   return (
     <>
       <Navbar />
-      <div className="delete-car-container">DeleteCar</div>
+      <div className="delete-car-container">
+        <h3 id="deleteTitle">Available Cars List</h3>
+        <table>
+          <thead>
+            <tr>
+              <th className="delete-car-name">
+                <div className="header-container">Car Name</div>
+              </th>
+              <th className="actions">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((car) => (
+              <tr key={car.id}>
+                <td className="delete-car-name">{car.name}</td>
+                <td className="actions">
+                  <button className="delete-carBtn" type="button" onClick={() => handleDeleteCar(car.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
