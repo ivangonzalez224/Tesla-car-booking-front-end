68648597d2590b379adf2d2ef12636717f96c980
Bruno Kambere <brunokambere@gmail.com>
2023-12-06 20:01:47 +0200

    Implement carousel component to MainPage.jsx

--- src/components/cars/MainPage.jsx
+++ src/components/cars/MainPage.jsx
@@ -2,29 +2,55 @@ import { React } from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from '../Navbar';
 import CarItem from './CarItem';
-import '../../assets/css/carousel.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import '../../assets/css/carouselStyle.css'
+
 
 function MainPage() {
   const { cars } = useSelector((state) => state.cars);
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 992 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 992, min: 320 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 320, min: 0 },
+      items: 1
+    }
+  };
 
   return (
     <>
       <Navbar />
-      <div className="main-page-container">MainPage</div>
+      <div className="main-page-container">
+      <Carousel responsive={responsive}>
       {cars.map((car) => (
-        <CarItem
-          key={car.id}
-          car={car}
-          classNames={{
-            button: 'btn btn-outline-primary',
-            carBody: 'card-body',
-            imageContainer: 'image-container',
-            image: 'card-img-top',
-            title: 'card-title',
-            discription: 'card-text',
-          }}
-        />
-      ))}
+            <div className='car-item'>
+              <CarItem
+                key={car.id}
+                car={car}
+                classNames={{
+                  button: 'btn btn-outline-primary btn-car-item',
+                  carBody: 'card-body',
+                  imageContainer: 'image-container',
+                  image: 'card-img-top',
+                  title: 'card-title',
+                  discription: 'card-text',
+                }}
+              />
+            </div>
+          ))}
+      </Carousel>;
+      </div>
     </>
   );
 }
