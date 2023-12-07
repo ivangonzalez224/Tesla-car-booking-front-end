18ef47a1375dee266d597c96536acec6c6756d2b
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-12-07 20:57:24 +0200

    Update reserveNav

--- src/components/reserve/ReserveNav.js
+++ src/components/reserve/ReserveNav.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
 function CityDropdown({ onSelectCity }) {
   const [selectedCountry, setSelectedCountry] = useState('');
@@ -17,8 +19,6 @@ function CityDropdown({ onSelectCity }) {
 
   useEffect(() => {
     if (selectedCountry) {
-      // Simulating fetching cities for the selected country
-      // In a real app, you would replace this with an API call for cities in the selected country
       setCities(['City 1', 'City 2', 'City 3']);
     }
   }, [selectedCountry]);
@@ -26,7 +26,7 @@ function CityDropdown({ onSelectCity }) {
   const handleCountryChange = (event) => {
     const country = event.target.value;
     setSelectedCountry(country);
-    onSelectCity(country, ''); // Reset selected city when the country changes
+    onSelectCity(country, '');
   };
 
   const handleCityChange = (event) => {
@@ -69,11 +69,26 @@ function CityDropdown({ onSelectCity }) {
   );
 }
 
+CityDropdown.propTypes = {
+  onSelectCity: PropTypes.func.isRequired,
+};
+
 function ReserveNav() {
   const { username } = JSON.parse(localStorage.getItem('Token')) || {};
   const { cars } = useSelector((state) => state.cars);
   const filteredCars = cars.filter((car) => !car.is_removed);
   const [selectedCity, setSelectedCity] = useState('');
+  const [selectedCar, setSelectedCar] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const handleSubmit = () => {
+    const reservationData = {
+      selectedCity,
+      selectedCar,
+      selectedDate,
+    };
+    console.log('Reservation Data:', reservationData);
+  };
 
   return (
     <div className="reserve-container">
@@ -85,12 +100,33 @@ function ReserveNav() {
           {username}
         </p>
         <CityDropdown onSelectCity={(country, city) => setSelectedCity(city)} />
-        Select a car:
-        <select id="" required>
-          {filteredCars.map((car) => (
-            <option key={car.id}>{car.name}</option>
-          ))}
-        </select>
+        <div>
+          Select a car:
+          <select
+            id="reserve-carSelect"
+            onChange={(e) => setSelectedCar(e.target.value)}
+            required
+          >
+            <option value="">Select a car</option>
+            {filteredCars.map((car) => (
+              <option key={car.id} value={car.name}>
+                {car.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          Select a date for the test:
+          <input
+            type="datetime-local"
+            id="reserve-date"
+            onChange={(e) => setSelectedDate(e.target.value)}
+            required
+          />
+        </div>
+        <button className="reserve-btn" type="button" onClick={handleSubmit}>
+          Reserve
+        </button>
       </div>
     </div>
   );
