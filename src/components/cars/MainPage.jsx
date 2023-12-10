f952fafb8c2fc7fbd00593cad3c04ba8336c420e
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-12-10 20:42:34 +0200

    Update main page style

--- src/components/cars/MainPage.jsx
+++ src/components/cars/MainPage.jsx
@@ -27,11 +27,11 @@ function MainPage() {
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 992, min: 320 },
+      breakpoint: { max: 992, min: 375 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 320, min: 0 },
+      breakpoint: { max: 375, min: 0 },
       items: 1,
     },
   };
