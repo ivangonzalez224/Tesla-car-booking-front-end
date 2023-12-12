70b38e1c119af31ed43a6119ed1186f207db7701
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-12-12 22:39:16 +0200

    Update app file with arrow function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
@@ -12,23 +13,21 @@ import CarDetails from './components/details/CarDetails';
 import ReserveNav from './components/reserve/ReserveNav';
 import ReserveDetails from './components/reserve/ReserveDetails';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Session />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route element={<RequireAuth />}>
-        <Route exact path="/addCar" element={<AddCarsForm />} />
-        <Route path="/mainPage" element={<MainPage />} />
-        <Route path="/details/:carId" element={<CarDetails />} />
-        <Route path="/cars/delete" element={<DeleteCar />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/reserveNav" element={<ReserveNav />} />
-        <Route path="/reserveDetails/:carId" element={<ReserveDetails />} />
-      </Route>
-    </Routes>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Session />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route element={<RequireAuth />}>
+      <Route exact path="/addCar" element={<AddCarsForm />} />
+      <Route path="/mainPage" element={<MainPage />} />
+      <Route path="/details/:carId" element={<CarDetails />} />
+      <Route path="/cars/delete" element={<DeleteCar />} />
+      <Route path="/reservations" element={<Reservations />} />
+      <Route path="/reserveNav" element={<ReserveNav />} />
+      <Route path="/reserveDetails/:carId" element={<ReserveDetails />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
